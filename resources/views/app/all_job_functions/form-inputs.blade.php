d041ef959bad49610dc1c59a1ce646f3af085365
victorbrescott@hotmail.com <Breskott>
2022-06-25 15:37:55 -0300

    Ajustes finais

--- resources/views/app/all_job_functions/form-inputs.blade.php
+++ resources/views/app/all_job_functions/form-inputs.blade.php
@@ -23,46 +23,46 @@
     </x-inputs.group>
 
     <x-inputs.group class="w-full lg:w-6/12">
-        <x-inputs.text
+        <x-inputs.time
             name="start_time"
             label="Horario de Inicio"
             value="{{ old('start_time', ($editing ? $jobFunctions->start_time : '')) }}"
             placeholder="Start Time"
             type="time"
             required
-        ></x-inputs.text>
+        ></x-inputs.time>
     </x-inputs.group>
 
     <x-inputs.group class="w-full lg:w-6/12">
-        <x-inputs.text
+        <x-inputs.time
             name="end_time"
             label="Horario Fim "
             value="{{ old('end_time', ($editing ? $jobFunctions->end_time : '')) }}"
             type="time"
             placeholder="End Time"
             required
-        ></x-inputs.text>
+        ></x-inputs.time>
     </x-inputs.group>
 
     <x-inputs.group class="w-full lg:w-6/12">
-        <x-inputs.text
+        <x-inputs.time
             name="time_output_interval"
             label="Saida Intervalo"
             value="{{ old('time_output_interval', ($editing ? $jobFunctions->time_output_interval : '')) }}"
             type="time"
             placeholder="Time Output Interval"
             required
-        ></x-inputs.text>
+        ></x-inputs.time>
     </x-inputs.group>
 
     <x-inputs.group class="w-full lg:w-6/12">
-        <x-inputs.text
+        <x-inputs.time
             name="time_entry_interval"
             label="Entrada Intervalo"
             value="{{ old('time_entry_interval', ($editing ? $jobFunctions->time_entry_interval : '')) }}"
             type="time"
             placeholder="Time Entry Interval"
             required
-        ></x-inputs.text>
+        ></x-inputs.time>
     </x-inputs.group>
 </div>
